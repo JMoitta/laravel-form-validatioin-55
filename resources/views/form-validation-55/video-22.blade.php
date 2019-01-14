34d05f96222e581c55e0800241ce40b540f33d8c
mex8063 <mex8063@24046-5636.INFO>
2019-01-14 12:58:12 -0200

    Criando campo para separar tipos de cliente

--- resources/views/form-validation-55/video-22.blade.php
+++ resources/views/form-validation-55/video-22.blade.php
@@ -1,3 +1,10 @@
-<h1>Modelando no banco de dados tipos de cliente</h1>
+<h1>Criando campo para separar tipos de cliente</h1>
+
+<h3>Criando nova Migração para separar tipos de cliente</h3>
+<p>Criação de nova migração para inserir o campo type na tabela clients.</p>
+<ul>
+    <li><b>php artisan make:migration add_client_type_field_to_clients-table --table=clients</b></li>
+</ul>
+
 
 <small>video 10:28</small>
