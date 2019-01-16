4b68eb48b81ac506e945fc08103882a61062b649
mex8063 <mex8063@24046-5636.INFO>
2019-01-16 13:34:55 -0200

    Validação do codigo verificador de CPF e CNPJ

---
+++ resources/views/form-validation-55/video-27.blade.php
@@ -0,0 +1,8 @@
+<h1>Validação do codigo verificador de CPF e CNPJ</h1>
+
+<h3>Instalando a biblioteca de vaidação CPF e CNPJ</h3>
+<p>Execute o comando abaixo para instalar o codeedu/code_validator:0.0.2</p>
+<ul>
+    <li><b>composer require codeedu/code_validator:0.0.2</b></li>
+</ul>
+<small>video completo</small>
